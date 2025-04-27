5409b4e09268b177d09348b56f17ffcb3de93390
최경영 <yeong7098@naver.com>
2025-04-27 16:08:28 +0900

    FEAT : FCM Contoller

--- src/fcm/fcm.controller.ts
+++ src/fcm/fcm.controller.ts
@@ -1,6 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { FcmService } from './fcm.service';
 
 @Controller('fcm')
 export class FcmController {
-  
+  constructor(private readonly fcmService : FcmService) {}
+
+  @Post('push')
+  async sendPush(@Body() body : { token : string, title : string, message : string }) {
+    const { token, title, message } = body;
+    const messageId = await this.fcmService.sendPushNotification(token, title, message);
+
+    return { messageId };
+  }
+
 }
--- src/fcm/fcm.module.ts
+++ src/fcm/fcm.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { FcmService } from './fcm.service';
+import { FcmController } from './fcm.controller';
 
-@Module({})
+@Module({
+  imports : [ConfigModule],
+  providers : [FcmService],
+  controllers : [FcmController],
+})
 export class FcmModule {}
